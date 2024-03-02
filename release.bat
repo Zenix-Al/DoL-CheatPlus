2d3a54514aa8b4b1b08530eba130397b22c94585
Zenix-Al <75069795+Zenix-Al@users.noreply.github.com>
2024-03-03 06:23:17 +0700

    Delete release.bat

--- release.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy "..\sources\_compiled\index.offline.html" "offline\DoL 0.4.5.3 - cheat non server version.html"
-copy "..\sources\_compiled\index.server.php" "server\index.php"
-copy changelog*.txt offline
-copy changelog*.txt server
-pause
