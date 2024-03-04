8c82ec02d355aa4b4e08f1c5ba063064905a9902
Zenix-Al <75069795+Zenix-Al@users.noreply.github.com>
2024-03-04 13:25:22 +0700

    update release
    Update release support for nwjs and online version

--- release/compile+release.bat
+++ release/compile+release.bat
@@ -1,19 +1,19 @@
 @echo off
 cd ../sources
-start cmd /c "_quick_compile.bat"
+start cmd /c "compile.bat"
 :wait
 timeout /t 1 /nobreak >NUL
-if not exist _compiled\index.offline.html goto wait
+if not exist _compiled\online\main.js goto wait
 cd ../release
 echo..
 echo..
 echo copying files to release..
-copy "..\sources\_compiled\index.offline.html" "offline\DoL 0.4.5.3 - cheat non server version.html"
-copy "..\sources\_compiled\index.server.php" "server\index.php"
-del offline\changelog*.txt
-del server\changelog*.txt
-copy changelog*.txt offline
-copy changelog*.txt server
+copy "..\sources\_compiled\nwjs" "nwjs\www\cheat"
+copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.5.3 - cheatPlus.html"
+copy "..\sources\_compiled\online\main.js" "online\online DoL - cheatPlus.js"
+copy "..\sources\_compiled\server\index.php" "server\index.php"
+copy "..\sources\_compiled\server\start.bat" "server\start.bat"
+copy "..\sources\_compiled\server\start-admin.bat" "server\start-admin.bat"
 echo done!
 echo.
 echo.
--- release/release.bat
+++ release/release.bat
@@ -1,6 +1,12 @@
 @echo off
-copy "..\sources\_compiled\index.offline.html" "offline\DoL 0.4.5.3 - cheat non server version.html"
-copy "..\sources\_compiled\index.server.php" "server\index.php"
-copy changelog*.txt offline
-copy changelog*.txt server
-pause
+echo copying files to release..
+copy "..\sources\_compiled\nwjs" "nwjs\www\cheat"
+copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.5.3 - cheatPlus.html"
+copy "..\sources\_compiled\online\main.js" "online\online DoL - cheatPlus.js"
+copy "..\sources\_compiled\server\index.php" "server\index.php"
+copy "..\sources\_compiled\server\start.bat" "server\start.bat"
+copy "..\sources\_compiled\server\start-admin.bat" "server\start-admin.bat"
+echo done!
+echo.
+echo.
+echo operation complete
