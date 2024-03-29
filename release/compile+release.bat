bd4fbcaf2b0a21fe4e279249ecdd67af863f2c35
Zenix-Al <75069795+Zenix-Al@users.noreply.github.com>
2024-03-29 12:23:48 +0700

    Update compile+release

--- release/compile+release.bat
+++ release/compile+release.bat
@@ -9,7 +9,7 @@ echo..
 echo..
 echo copying files to release..
 copy "..\sources\_compiled\nwjs" "nwjs\www\cheat"
-copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.5.3 - cheatPlus.html"
+copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.6.3 - cheatPlus.html"
 copy "..\sources\_compiled\online\main.js" "online\online DoL - cheatPlus.js"
 copy "..\sources\_compiled\server\index.php" "server\index.php"
 copy "..\sources\_compiled\server\start.bat" "server\start.bat"
