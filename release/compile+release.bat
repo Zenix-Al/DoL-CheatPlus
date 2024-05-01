3e7e1b01ebda4856797f1b8f1203f634376112c3
Zenix-Al <75069795+Zenix-Al@users.noreply.github.com>
2024-05-01 22:41:05 +0700

    compiler

--- release/compile+release.bat
+++ release/compile+release.bat
@@ -5,16 +5,7 @@ start cmd /c "compile.bat"
 timeout /t 1 /nobreak >NUL
 if not exist _compiled\online\main.js goto wait
 cd ../release
-echo..
-echo..
-echo copying files to release..
-copy "..\sources\_compiled\nwjs" "nwjs\www\cheat"
-copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.6.3 - cheatPlus.html"
-copy "..\sources\_compiled\online\main.js" "online\online DoL - cheatPlus.js"
-copy "..\sources\_compiled\server\index.php" "server\index.php"
-copy "..\sources\_compiled\server\start.bat" "server\start.bat"
-copy "..\sources\_compiled\server\start-admin.bat" "server\start-admin.bat"
-echo done!
 echo.
 echo.
-echo operation complete
+release.bat
+
--- release/release.bat
+++ release/release.bat
@@ -1,7 +1,9 @@
 @echo off
+echo current version of the game :
+set /p version=
 echo copying files to release..
 copy "..\sources\_compiled\nwjs" "nwjs\www\cheat"
-copy "..\sources\_compiled\offline\game.html" "offline\DoL 0.4.5.3 - cheatPlus.html"
+copy "..\sources\_compiled\offline\game.html" "offline\DoL %version% - cheatPlus.html"
 copy "..\sources\_compiled\online\main.js" "online\online DoL - cheatPlus.js"
 copy "..\sources\_compiled\server\index.php" "server\index.php"
 copy "..\sources\_compiled\server\start.bat" "server\start.bat"
